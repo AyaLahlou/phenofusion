68d0670894c531e51e2cd5b7635dabb7b5bfef84
Maria Fernanda Sampaio Ferreira <ms7073@columbia.edu>
2025-10-06 19:00:21 -0400

    bugfix: update slurm output file for process_SHR

--- bashscripts/process_SHR.sh
+++ bashscripts/process_SHR.sh
@@ -6,7 +6,7 @@
 #
 #SBATCH --account=glab        # Replace ACCOUNT with your group account name
 #SBATCH --job-name=process    # The job name
-#SBATCH --output=process_gra.out    # The output file name
+#SBATCH --output=process_shr.out    # The output file name
 #SBATCH -c 2                   # The number of cpu cores to use (up to 32 cores per server)
 #SBATCH --mem-per-cpu=160G        # The memory the job will use per cpu core
 #SBATCH --time=0-24:00            # The time the job will take to run in D-HH:MM
